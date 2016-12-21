d39d773dd9a785f8099e1bc55eea01fbd9a22254
DrewCCAD <drewccad@gmail.com>
2016-12-21 15:30:50 -0600

    11 Gist Blog Final
    all working with:
    -css
    -bootstrap
    -glyphicons in script...

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -2,12 +2,11 @@
 
 $(document).ready(function() {
     // Spec 1.1
-    // Using jQuery to make an AJAX call, insert a list of links into #gists using JavaScript forEach with the "description" of each gist as the text.
-    // -#id of each blogPost is the 'discription'
-
+    // Using jQuery to make an AJAX call, insert a list of links into #gists using JavaScript forEach
+    // with the "description" of each gist as the text.-#id of each blogPost is the 'discription'
     // Spec 1.2
-    // Within each loop, write an if condition to insert only gists that start with a '#gist' in the "description". After fitering, remove the '#gist ' from the title. (use sub-string to remove stuff with the #gist.)
-
+    // Within each loop, write an if condition to insert only gists that start with a '#gist' in the "description".
+    // After fitering, remove the '#gist ' from the title. (use sub-string to remove stuff with the #gist.)
     // Spec 1.3
     // Set the href attribute on each link to "#", and a "data-url" attribute equal to the "url" value. (like what did in address book)
 
@@ -16,19 +15,23 @@ $(document).ready(function() {
             gists.forEach(function(gist) {
                 // make an if statement look discription start with #gist
                 if (gist.description.startsWith('#post')) {
-                    var blog = $('<li><a href="#" data-url="' + gist.url + '">' + gist.description.substring(6) + '</a></li>');
+                    var blog = $('<li><i class="glyphicon glyphicon-comment"></i><a href="#" data-url="' + gist.url + '">' + gist.description.substring(6) + '</a></li>');
                     $('#posts').append(blog);
                 }
             });
         }
     });
     // Spec 2
-    // After the links are are inserted, add a click listener, and prevent the default event from occuring. Then make an ajax call with the "data-url" value, grabbing it with $.data('url'). (like lines of 24-30 in address book code)
+    // After the links are are inserted, add a click listener, and prevent the default event from occuring.
+    // Then make an ajax call with the "data-url" value, grabbing it with $.data('url'). (like lines of 24-30 in address book code)
     // Spec 3
     // Insert the "content" of the file named gist.md into #gist.
-
     // Spec 3.1
-    // Since our "content" is written in Markdown, we can use the Marked.js library to convert the content to html. Simply call marked() on your content. e.g. marked(content).
+    // Since our "content" is written in Markdown, we can use the Marked.js library to convert the content to html. Simply call marked() on your content.
+    // e.g. marked(content).
+    // Spec 4
+    // After inserting your content, make another ajax call using the "comments_url", and insert the ["blogPost"]["login"]
+    // and "body" in a list in #comments. *(make 3 ajax call one to main url then second to url, third to comment url)
 
     $('#posts').on('click', 'a', function(link) {
         link.preventDefault();
@@ -36,20 +39,15 @@ $(document).ready(function() {
         $.ajax(clicked, {
             success: function(url) {
                 var postContent = marked(url.files['post.md'].content);
-                $('#post').append(postContent);
+                $('#post').html(postContent);
                 console.log(postContent);
                 $.ajax(url['comments_url'], {
                     success: function(comment) {
-                        $('#comments').html('<h4>' + comment[0]['user']['login'] + '</h4><p>' + comment[0].body + '</p>');
+                        $('#comments').html('<h3><i class="glyphicon glyphicon-user"></i>' + comment[0]['user']['login'] + '</h3><li>' + comment[0].body + '</li>');
                     },
                 });
             }
         })
     });
+    // end doc ready function
 });
-
-// Spec 4
-// After inserting your content, make another ajax call using the "comments_url", and insert the ["blogPost"]["login"] and "body" in a list in #comments. *(make 3 ajax call one to main url then second to url, third to comment url)
-
-// Spec 5
-// Make it look nice! This is going to be YOUR blog!
